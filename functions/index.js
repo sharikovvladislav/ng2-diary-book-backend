aa6647c8983b459a818c00b5b0fbbc7a9c236f1c
Sharikov Vladislav <sharikov.vladislav@gmail.com>
2017-07-28 13:01:16 +0300

    remove uuid + fix git ignore

--- functions/index.js
+++ functions/index.js
@@ -25,6 +25,141 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 // [END import]
 
+// [START functionsimport]
+// [END functionsimport]
+// [START additionalimports]
+// Moments library to format dates.
+const moment = require('moment');
+// CORS Express middleware to enable CORS Requests.
+const cors = require('cors')({origin: true});
+
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const app = express();
+// [END additionalimports]
+
+// [START all]
+/**
+ * Returns the server's date. You must provide a `format` URL query parameter or `format` vaue in
+ * the request body with which we'll try to format the date.
+ *
+ * Format must follow the Node moment library. See: http://momentjs.com/
+ *
+ * Example format: "MMMM Do YYYY, h:mm:ss a".
+ * Example request using URL query parameters:
+ *   https://us-central1-<project-id>.cloudfunctions.net/date?format=MMMM%20Do%20YYYY%2C%20h%3Amm%3Ass%20a
+ * Example request using request body with cURL:
+ *   curl -H 'Content-Type: application/json' /
+ *        -d '{"format": "MMMM Do YYYY, h:mm:ss a"}' /
+ *        https://us-central1-<project-id>.cloudfunctions.net/date
+ *
+ * This endpoint supports CORS.
+ */
+exports.onUserCreate = functions.auth.user().onCreate(event => {
+  const user = event.data; // The Firebase user.
+
+
+  return createUser(user);
+});
+
+function createUser(user) {
+  const usersRef = admin.database().ref('/users');
+
+  const newUser = {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName
+  };
+
+  return usersRef.push(newUser);
+}
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = (req, res, next) => {
+  console.log('Check if request is authorized with Firebase ID token');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+      !req.cookies.__session) {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.');
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  }
+  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+  }).catch(error => {
+    console.error(`Error while verifying Firebase ID token: ${idToken}, error: ${error}`);
+    res.status(403).send('Unauthorized');
+  });
+};
+
+app.use(cors);
+app.use(cookieParser);
+app.use(validateFirebaseIdToken);
+app.get('/hello', (req, res) => {
+  res.send({text: `Hello ${req.user.name}`, user: req.user});
+});
+
+// This HTTPS endpoint can only be accessed by your Firebase Users.
+// Requests need to be authorized by providing an `Authorization` HTTP header
+// with value `Bearer <Firebase ID Token>`.
+exports.app = functions.https.onRequest(app);
+
+// [START trigger]
+exports.date = functions.https.onRequest((req, res) => {
+// [END trigger]
+  // [START sendError]
+  // Forbidding PUT requests.
+  if (req.method === 'PUT') {
+    res.status(403).send('Forbidden!');
+  }
+  // [END sendError]
+
+  // [START usingMiddleware]
+  // Enable CORS using the `cors` express middleware.
+  cors(req, res, () => {
+  // [END usingMiddleware]
+    // Reading date format from URL query parameter.
+    // [START readQueryParam]
+    let format = req.query.format;
+    // [END readQueryParam]
+    // Reading date format from request body query parameter
+    if (!format) {
+      // [START readBodyParam]
+      format = req.body.format;
+      // [END readBodyParam]
+    }
+    // [START sendResponse]
+    const formattedDate = moment().format(format);
+    console.log('Sending Formatted date:', {
+      date: formattedDate
+    });
+    res.status(200).send({
+      date: formattedDate
+    });
+    // [END sendResponse]
+  });
+});
+// [END all]
+
 // [START makeUppercase]
 // [START makeUppercaseTrigger]
 exports.createFriendship = functions.database
